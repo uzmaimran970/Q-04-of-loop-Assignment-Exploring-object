f9a20364e598f9a50c2891627f9fd422abac2773
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-06-04 20:21:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+// 4.Exploring Objects with for...in Loop
+// Objective: Practice working with objects in TypeScript and iterating over their properties using
+// a for...in loop.
+// Instructions:
+// 1. Create a simple object with three items:
+// o Define an object called myObject with three properties: item1, item2, and item3,
+// each with corresponding string values.
+// 2. Use a for...in loop to get properties' names and values from the object:
+// o Iterate through the properties of myObject using a for...in loop.
+// o Inside the loop, print each property's name and its corresponding value to the 
+// console.
+let myObject = {
+    item1: "Laptop",
+    item2: "fabric",
+    item3: "perfume",
+};
+for (let properties in myObject) {
+    console.log(`${properties} ,${myObject[properties]}`);
+}
---
+++ index.ts
@@ -0,0 +1,21 @@
+// 4.Exploring Objects with for...in Loop
+// Objective: Practice working with objects in TypeScript and iterating over their properties using
+// a for...in loop.
+// Instructions:
+// 1. Create a simple object with three items:
+// o Define an object called myObject with three properties: item1, item2, and item3,
+// each with corresponding string values.
+// 2. Use a for...in loop to get properties' names and values from the object:
+// o Iterate through the properties of myObject using a for...in loop.
+// o Inside the loop, print each property's name and its corresponding value to the 
+// console.
+let myObject:any = {
+    item1: "Laptop" ,
+    item2: "Shoes" , 
+    item3: "perfume" ,
+};
+
+for (let properties in myObject){
+    console.log(`${properties} ,${myObject[properties]}`);
+    
+}
